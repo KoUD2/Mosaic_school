944c7d06ef026e0a8bb74f3cb4492c641bd4c9f3
KoUD2 <kostik-udod@mail.ru>
2023-06-24 19:47:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,344 @@
+window.onload=function(){
+  const element = document.getElementsByClassName("input1");
+  const element2 = document.getElementsByClassName("input2");
+  const element3 = document.getElementsByClassName("input3");
+  const element4 = document.getElementsByClassName("input4");
+  const element5 = document.getElementsByClassName("input5");
+  const element6 = document.getElementsByClassName("input6");
+  const element7 = document.getElementsByClassName("input7");
+  const element8 = document.getElementsByClassName("input8");
+  const element9 = document.getElementsByClassName("input9");
+  const element10 = document.getElementsByClassName("input10");
+  const element11 = document.getElementsByClassName("input11");
+  const element12 = document.getElementsByClassName("input12");
+  const element13 = document.getElementsByClassName("input13");
+  const element14 = document.getElementsByClassName("input14");
+  const element15 = document.getElementsByClassName("input15");
+  const element16 = document.getElementsByClassName("input16");
+  const element17 = document.getElementsByClassName("input17");
+  const element18 = document.getElementsByClassName("input18");
+  element[0].addEventListener('keypress', function(e){
+        if(e.which === 13){
+          e.preventDefault();
+          let elem1 = document.getElementsByClassName("p1")
+          let val1 = document.getElementsByClassName("input1")[0].value;
+          if (val1 == 'Elizabeth' || val1 == 'elizabeth') {
+            elem1[0].style.opacity = '0';
+            setTimeout(function(){
+              elem1[0].style.display = 'none';
+            }, 1000);
+          } else {
+            element[0].style.border = '#FF563F solid 0.156vw';
+            setTimeout(function(){
+              element[0].style.border = '#FFA23F solid 0.156vw';
+            }, 2000);
+          }
+        }
+  });
+  element2[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem2 = document.getElementsByClassName("p2")
+      let val2 = document.getElementsByClassName("input2")[0].value;
+      if (val2 == 'Asia' || val2 == 'asia') {
+        elem2[0].style.opacity = '0';
+        setTimeout(function(){
+          elem2[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element2[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element2[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element3[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem3 = document.getElementsByClassName("p3")
+      let val3 = document.getElementsByClassName("input3")[0].value;
+      if (val3 == '18' || val3 == 'eighteenth' || val3 == 'Eighteenth' || val3 == 'XVIII century' || val3 == 'eighteenth century' || val3 == 'Eighteenth century' || val3 == 'eighteenth Century' || val3 == 'Eighteenth Century' || val3 == 'XVIII') {
+        elem3[0].style.opacity = '0';
+        setTimeout(function(){
+          elem3[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element3[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element3[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element4[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem4 = document.getElementsByClassName("p4")
+      let val4 = document.getElementsByClassName("input4")[0].value;
+      if (val4 == 'Cheetah' || val4 == 'cheetah') {
+        elem4[0].style.opacity = '0';
+        setTimeout(function(){
+          elem4[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element4[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element4[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element5[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem5 = document.getElementsByClassName("p5")
+      let val5 = document.getElementsByClassName("input5")[0].value;
+      if (val5 == 'Penicillin' || val5 == 'penicillin') {
+        elem5[0].style.opacity = '0';
+        setTimeout(function(){
+          elem5[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element5[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element5[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element6[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem6 = document.getElementsByClassName("p6")
+      let val6 = document.getElementsByClassName("input6")[0].value;
+      if (val6 == 'Washington' || val6 == 'washington' || val6 == 'D.C.' || val6 == 'DC.' || val6 == 'D.C' || val6 == 'DC' || val6 == 'd.c.' || val6 == 'dc.' || val6 == 'd.c' || val6 == 'dc' || val6 == 'd.C.' || val6 == 'dC.' || val6 == 'd.C' || val6 == 'dC' || val6 == 'D.c.' || val6 == 'Dc.' || val6 == 'D.c' || val6 == 'Dc') {
+        elem6[0].style.opacity = '0';
+        setTimeout(function(){
+          elem6[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element6[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element6[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element7[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem7 = document.getElementsByClassName("p7")
+      let val7 = document.getElementsByClassName("input7")[0].value;
+      if (val7 == 'February' || val7 == 'february' || val7 == '2') {
+        elem7[0].style.opacity = '0';
+        setTimeout(function(){
+          elem7[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element7[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element7[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element8[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem8 = document.getElementsByClassName("p8")
+      let val8 = document.getElementsByClassName("input8")[0].value;
+      if (val8 == 'William Shakespeare' || val8 == 'william shakespeare' || val8 == 'William shakespeare' || val8 == 'william Shakespeare' || val8 == 'Shakespeare') {
+        elem8[0].style.opacity = '0';
+        setTimeout(function(){
+          elem8[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element8[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element8[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element9[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem9 = document.getElementsByClassName("p9")
+      let val9 = document.getElementsByClassName("input9")[0].value;
+      if (val9 == 'Bishop' || val9 == 'bishop') {
+        elem9[0].style.opacity = '0';
+        setTimeout(function(){
+          elem9[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element9[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element9[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element10[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem10 = document.getElementsByClassName("p10")
+      let val10 = document.getElementsByClassName("input10")[0].value;
+      if (val10 == 'Sun' || val10 == 'sun') {
+        elem10[0].style.opacity = '0';
+        setTimeout(function(){
+          elem10[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element10[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element10[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element11[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem11 = document.getElementsByClassName("p11")
+      let val11 = document.getElementsByClassName("input11")[0].value;
+      if (val11 == 'Cannonball' || val11 == 'cannonball') {
+        elem11[0].style.opacity = '0';
+        setTimeout(function(){
+          elem11[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element11[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element11[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element12[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem12 = document.getElementsByClassName("p12")
+      let val12 = document.getElementsByClassName("input12")[0].value;
+      if (val12 == 'Desdemona' || val12 == 'desdemona') {
+        elem12[0].style.opacity = '0';
+        setTimeout(function(){
+          elem12[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element12[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element12[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element13[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem13 = document.getElementsByClassName("p13")
+      let val13 = document.getElementsByClassName("input13")[0].value;
+      if (val13 == 'Diogenes' || val13 == 'diogenes') {
+        elem13[0].style.opacity = '0';
+        setTimeout(function(){
+          elem13[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element13[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element13[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element14[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem14 = document.getElementsByClassName("p14")
+      let val14 = document.getElementsByClassName("input14")[0].value;
+      if (val14 == '100' || val14 == '100deg' || val14 == '100degrees' || val14 == '100 degrees' || val14 == '100 deg') {
+        elem14[0].style.opacity = '0';
+        setTimeout(function(){
+          elem14[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element14[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element14[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element15[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem15 = document.getElementsByClassName("p15")
+      let val15 = document.getElementsByClassName("input15")[0].value;
+      if (val15 == 'Krypton' || val15 == 'krypton') {
+        elem15[0].style.opacity = '0';
+        setTimeout(function(){
+          elem15[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element15[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element15[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element16[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem16 = document.getElementsByClassName("p16")
+      let val16 = document.getElementsByClassName("input16")[0].value;
+      if (val16 == 'Tom Cruise' || val16 == 'tom cruise' || val16 == 'Cruise' || val16 == 'cruise') {
+        elem16[0].style.opacity = '0';
+        setTimeout(function(){
+          elem16[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element16[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element16[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element17[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem17 = document.getElementsByClassName("p17")
+      let val17 = document.getElementsByClassName("input17")[0].value;
+      if (val17 == 'Tchaikovsky' || val17 == 'tchaikovsky') {
+        elem17[0].style.opacity = '0';
+        setTimeout(function(){
+          elem17[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element17[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element17[0].style.border = '#FFF solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+  element18[0].addEventListener('keypress', function(e){
+    if(e.which === 13){
+      e.preventDefault();
+      let elem18 = document.getElementsByClassName("p18")
+      let val18 = document.getElementsByClassName("input18")[0].value;
+      if (val18 == '4' || val18 == 'four' || val18 == 'Four') {
+        elem18[0].style.opacity = '0';
+        setTimeout(function(){
+          elem18[0].style.display = 'none';
+        }, 1000);
+      } else {
+        element18[0].style.border = '#FF563F solid 0.156vw';
+        setTimeout(function(){
+          element18[0].style.border = '#FFA23F solid 0.156vw';
+        }, 2000);
+      }
+    }
+  });
+}
